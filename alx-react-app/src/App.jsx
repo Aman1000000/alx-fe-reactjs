81c4b4a1656cd1ccfeffa8d3a32c51cf268eeea5
Aman1000000 <yimeramanuel2@gmail.com>
2024-08-11 08:10:01 -0700

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,10 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header'
-import MainContent from './Maincontent'
-import Footer from './Footer'
+
 import UserProfile from './components/UserProfile'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 
 
