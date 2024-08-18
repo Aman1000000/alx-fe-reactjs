9753dabca3e35be060d8d050289ff0ead7aa81a7
Aman1000000 <yimeramanuel2@gmail.com>
2024-08-18 08:03:02 -0700

    fix app

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ProfilePage from './component/ProfilePage.jsx'
-import UserDetails from './component/UserDetails.jsx'
-import UserInfo from './component/UserInfo.jsx'
+
 import UserContext from './component/UserContext.js'
 function App() {
   const userData = { name: "Jane Doe", age: 30 };
@@ -18,3 +13,4 @@ function App() {
     </UserContext.Provider>
   );
 }
+export default App;
