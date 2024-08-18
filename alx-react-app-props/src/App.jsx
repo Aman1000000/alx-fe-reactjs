5e66dfbc4d593bd49d65f90b4ff0c0a56c6cf62d
Aman1000000 <yimeramanuel2@gmail.com>
2024-08-18 07:23:37 -0700

    work

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ProfilePage from './ProfilePage.jsx'
-import UserDetails from './UserDetails.jsx'
-import UserInfo from './Userinfo.jsx'
+import ProfilePage from './component/ProfilePage.jsx'
+import UserDetails from './component/UserDetails.jsx'
+import UserInfo from './component/UserInfo.jsx'
 import UserContext from './userContext.js'
 function App() {
   const userData = { name: "Jane Doe", age: 30 };
