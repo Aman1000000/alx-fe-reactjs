5bd074e9403ee8378cb1b1e1b8ba3d6c250942c2
Aman1000000 <yimeramanuel2@gmail.com>
2024-08-18 07:44:40 -0700

    let's go

--- alx-react-app-props/src/component/ProfilePage.jsx
+++ alx-react-app-props/src/component/ProfilePage.jsx
@@ -1,10 +1,6 @@
-function UserDetails({ userData }) {
-  return (
-    <div>
-      <p>Name: {userData.name}</p>
-      <p>Email: {userData.email}</p>
-    </div>
-  );
+import UserInfo from "./UserInfo";
+function ProfilePage({ userData }) {
+  return <UserInfo userData={userData} />;
 }
 
-export default UserDetails;
+export default ProfilePage;
