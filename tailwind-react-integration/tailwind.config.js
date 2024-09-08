763e4b79608b2367c8f391e88924decb2e3b4e09
Aman1000000 <yimeramanuel2@gmail.com>
2024-09-08 11:00:36 -0700

    new

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './index.html', 
+  ],
+  darkMode: 'media', 
+ 
+  content: [
+    './src/**/*.{html,js,jsx}',
+    './public/index.html',
+  ],
+}
