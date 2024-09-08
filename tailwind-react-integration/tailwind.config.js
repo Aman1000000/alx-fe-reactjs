164aa71dc1ce8125240d264963376b754d2d9fd7
Aman1000000 <yimeramanuel2@gmail.com>
2024-09-08 11:39:43 -0700

    fix

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     './src/**/*.{html,js,jsx,ts,tsx}', // Adjust paths as needed
     './public/index.html',
   ],
+  darkMode: 'media', // or 'class'
   theme: {
     extend: {},
   },
