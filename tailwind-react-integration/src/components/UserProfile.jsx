051397fad38a057b403ab51c940fd7edf13167a6
Aman1000000 <yimeramanuel2@gmail.com>
2024-09-08 11:57:10 -0700

    fix

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,5 +1,3 @@
-// src/components/UserProfile.jsx
-
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
@@ -8,7 +6,7 @@ function UserProfile() {
         alt="User" 
         className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto" 
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-lg sm:text-xl text-blue-800 my-4">kemal konan</h1>
       <p className="text-sm sm:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
